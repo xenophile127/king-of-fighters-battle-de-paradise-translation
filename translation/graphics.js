1d53030ee3db785dc3919b5db78823cee1f13cbc
Marc Robledo <mark@tinet.org>
2025-10-27 20:32:52 +0100

    fixed TOO EARLY! map in syncro-fist

--- translation/graphics.js
+++ translation/graphics.js
@@ -74,7 +74,7 @@ const GRAPHIC_REPLACEMENTS=[
 
 	{type:'tileset', offset:0x10fce8, nTiles:12, comment:'Minigames - Let\'s dance - Performance', file:'minigame_lets_dance_performance'},
 	{type:'tileset', offset:0x1a4602, nTiles:28, comment:'Minigames - Bouzu Mekuri', file:'minigame_bouzu_mekuri'},
-	{type:'tileset', offset:0x0f0432, nTiles:29, comment:'Minigames - Syncro-fist', file:'minigame_syncro_fist'},
+	{type:'map', offsetTileset:0x0f0432, nTiles:29, offsetMap:0x0cb190, width:10, height:3, comment:'Minigames - Syncro-fist', file:'minigame_syncro_fist'},
 
 	{type:'map', offsetTileset:0x1c94a6, nTiles:158, offsetMap:0x193c64 + ((20*19*2) * 0), width:20, height:19, comment:'Static screen - Game Rules', file:'game_rules/map_game_rules_0'},
 	{type:'map', offsetTileset:0x1d3712, nTiles:225, offsetMap:0x193c64 + ((20*19*2) * 1), width:20, height:19, comment:'Static screen - Game rules 1.1', file:'game_rules/map_game_rules_1_1'},
